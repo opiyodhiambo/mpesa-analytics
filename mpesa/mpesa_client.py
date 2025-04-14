d37619a1e815e6941f482bdb66e7823c4bc2ba58
opiyodhiambo <rnldlinus@gmail.com>
2025-04-14 16:46:47 +0300

    register confirmation and validation urls

--- mpesa/mpesa_client.py
+++ mpesa/mpesa_client.py
@@ -3,37 +3,44 @@ from requests.auth import HTTPBasicAuth
 
 
 class MpesaClient:
-    def __init__(self, consumer_key, consumer_secret, shortcode, confirmation_url, validation_url=None):
+    def __init__(self, consumer_key, consumer_secret, shortcode, confirmation_url, validation_url=None, access_token=None):
         self.consumer_key = consumer_key
-        self.consumer_secret = consumer_secret,
+        self.consumer_secret = consumer_secret
         self.shortcode = shortcode
         self.confirmation_url = confirmation_url
         self.validation_url = validation_url
-        self.access_token = None
+        self.access_token = access_token
         self.base_url = "https://sandbox.safaricom.co.ke"
 
-    def authenticate(self):
+    def generate_access_token(self):
+
         authentication_url = f"{self.base_url}/oauth/v1/generate?grant_type=client_credentials"
-        response = requests.get(authentication_url, auth=HTTPBasicAuth(consumer_key, consumer_secret))
+        response = requests.get(authentication_url, auth=HTTPBasicAuth(self.consumer_key, self.consumer_secret))
+        print("Status Code:", response.status_code)
+        print("Response Text:", response.text)
+
         response.raise_for_status()
         self.access_token = response.json().get("access_token")
 
     def register_urls(self, response_type="Completed"):
         if not self.access_token:
-            self.authenticate()
+            self.generate_access_token()
 
         url = f"{self.base_url}/mpesa/c2b/v1/registerurl"
         headers = {             
-            "Authorization": f"Bearer {access_token}",
+            "Authorization": f"Bearer {self.access_token}",
             "Content-Type": "application/json"
         }
 
         body = {    
-            "ShortCode": shortcode,
+            "ShortCode": self.shortcode,
             "ResponseType": response_type,
-            "ConfirmationURL":confirmation_url
+            "ConfirmationURL":self.confirmation_url,
+            "ValidationURL": self.validation_url
         }
-
+        print("Sending registration payload:", body)
         response = requests.post(url, json=body, headers=headers)
+        print("Mpesa Response ::", response)
+        print("Response Text ::", response.text)
         response.raise_for_status()
         return response.json
--- mpesa/register.py
+++ mpesa/register.py
@@ -1,16 +1,23 @@
 from mpesa_client import MpesaClient
+from dotenv import load_dotenv
+import os
 
-consumer_key = ""
-consumer_secret = ""
-shortcode = ""
-response_type = "Completed"
-confirmation_url = ""
+load_dotenv()
+
+consumer_key = os.getenv('CONSUMER_KEY')
+consumer_secret = os.getenv('CONSUMER_SECRET')
+shortcode = os.getenv('SHORTCODE')
+response_type = os.getenv('RESPONSE_TYPE')
+confirmation_url = os.getenv('CONFIRMATION_URL')
+validation_url = os.getenv('VALIDATION_URL')
 
 client = MpesaClient(
     consumer_key=consumer_key,
     consumer_secret=consumer_secret,
     shortcode=shortcode,
-    confirmation_url=confirmation_url
+    confirmation_url=confirmation_url,
+    validation_url=validation_url,
+    access_token=None
 )
 
 if __name__ == "__main__":
